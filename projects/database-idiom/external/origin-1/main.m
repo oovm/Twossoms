5d366368321198109440110e154b35269a54b1a8
Aster <galaster@foxmail.com>
2019-02-25 23:20:21 +0800

    函数::记录下载时间

--- projects/database-idiom/external/origin-1/main.m
+++ projects/database-idiom/external/origin-1/main.m
@@ -4,24 +4,36 @@
 (*Setting*)
 
 
-SetDirectory@NotebookDirectory[];
+$reference = "https://github.com/by-syk/chinese-idiom-db";
+$here = NotebookDirectory[];
+$now = Now;
 
 
 (* ::Section:: *)
 (*Data*)
+$tasks = {
+	{
+		"download.mx",
+		"https://github.com/by-syk/chinese-idiom-db/raw/master/chinese-idioms-12976.txt"
+	}
+};
+check[local_, remote_] := GeneralUtilities`Scope[
+	file = FileNameJoin[{$here, local}];
+	If[
+		!FileExistsQ@file,
+		URLDownloadSubmit[remote, file],
+		Return@Nothing
+	]
+]
+TaskWait[check @@@ $tasks];
+$download = Now;
 
 
-data1 := data1 = GeneralUtilities`Scope[
-	Print@Hyperlink["https://github.com/by-syk/chinese-idiom-db"];
-	r := URLDownload[
-		"https://github.com/by-syk/chinese-idiom-db/raw/master/chinese-idioms-12976.txt",
-		"Source_1.mx"
-	];
-	If[!FileExistsQ@"Source_1.mx", r];
+
 	tmp = Import["Source_1.mx", "CSV"];
 	Echo[Length@tmp, "Records:"];
 	tmp[[All, {2, 3, 4}]]
-];
+
 
 
 data = MapAt[StringRiffle@*StringSplit, Join[data1, data2], {All, 2}];
