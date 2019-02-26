63af54c4878c556c06b564316c3ce28af82057bf
Aster <galaster@foxmail.com>
2019-02-27 00:03:38 +0800

    项目::宋词整理

---
+++ projects/database-song/external/origin-1/main.m
@@ -0,0 +1,61 @@
+(* ::Package:: *)
+
+(* ::Section:: *)
+(*Settings*)
+
+
+$source = "https://github.com/by-syk/chinese-idiom-db";
+$here = NotebookDirectory[];
+$now = Now;
+
+
+(* ::Section:: *)
+(*Data*)
+
+
+(* ::Subsection:: *)
+(*Tasks*)
+
+
+$tasks = {
+	{
+		"download.mx",
+		"https://github.com/by-syk/chinese-idiom-db/raw/master/chinese-idioms-12976.txt"
+	}
+};
+
+
+(* ::Subsection:: *)
+(*Download*)
+
+
+check[local_, remote_] := GeneralUtilities`Scope[
+	file = FileNameJoin[{$here, local}];
+	If[
+		!FileExistsQ@file,
+		URLDownloadSubmit[remote, file],
+		Return@Nothing
+	]
+];
+TaskWait[check @@@ $tasks];
+$download = Now;
+
+
+(* ::Subsection:: *)
+(*Export*)
+
+
+read[local_] := Import[FileNameJoin[{$here, local}], "RawJson"];
+Block[
+	{data},
+	data = Apply[Join, read@*First /@ $tasks][[All, {2, 3, 4}]];
+	data = MapAt[StringRiffle@*StringSplit, data, {All, 2}];
+	data = SortBy[Append[#, ""]& /@ DeleteDuplicatesBy[data, First], Rest];
+	Export[
+		FileNameJoin[{DirectoryName@$here, FileBaseName@$here <> ".mx"}],
+		data, "CSV",
+		"TableHeadings" -> {"Idiom", "Pinyin", "Explanation"},
+		CharacterEncoding -> "UTF8"
+	]
+];
+$finish = Now;
